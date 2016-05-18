3ba3270af85b2d4f9ffa42235fddf8b8b5385de2
Lonko <giovanni.caceres.rubio@gmail.com>
2016-05-18 00:29:47 -0300

    arreglos de inicio de session

--- index.php
+++ index.php
@@ -29,21 +29,20 @@
             <p>Algo debo poner aqui 
                 <!--Continually expanded and constantly improved Inspinia Admin Them (IN+)-->
             </p>
-            <p>Login in. To see it in action.</p>
+            <p>Iniciar sesión</p>
             <form class="m-t" role="form" action="portal.html">
                 <div class="form-group">
-                    <input type="email" class="form-control" placeholder="Username" required="">
+                    <input type="text" class="form-control" placeholder="Usuario" required="">
                 </div>
                 <div class="form-group">
-                    <input type="password" class="form-control" placeholder="Password" required="">
+                    <input type="password" class="form-control" placeholder="Contraseña" required="">
                 </div>
                 <button type="submit" class="btn btn-primary block full-width m-b">Login</button>
 
-                <a href="#"><small>Forgot password?</small></a>
-                <p class="text-muted text-center"><small>Do not have an account?</small></p>
-                <a class="btn btn-sm btn-white btn-block" href="register.html">Create an account</a>
+                <a href="#"><small>Olvido su contraseña?</small></a>
+              
             </form>
-            <p class="m-t"> <small>Inspinia we app framework base on Bootstrap 3 &copy; 2014</small> </p>
+            <p class="m-t"> <small><a href="gcrinformatica.cl">GcrInformatica</a> &copy; 2016</small> </p>
         </div>
     </div>
 
